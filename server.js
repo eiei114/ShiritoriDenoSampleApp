3395d564e0869be2c265329515dffbbf0fbbff5e
eiei114 <nowaka37564@gmail.com>
2023-02-16 13:25:43 +0900

    add 同じ単語は打てないようにする、ひらがな以外の文字を受け付けない

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ import {serve} from "https://deno.land/std@0.138.0/http/server.ts"
 import {serveDir} from "https://deno.land/std@0.138.0/http/file_server.ts";
 
 let previousWord = "しりとり";
+//文字列型の配列を作成
+const wordsList = [previousWord];
 
 console.log("Listening on http://localhost:8000");
 serve(async req => {
@@ -9,18 +11,35 @@ serve(async req => {
     console.log(pathname);
 
     if (req.method === "GET" && pathname === "/shiritori") {
-        return new Response(previousWord);
+        return new Response(previousWord,wordsList);
     }
     if(req.method === "POST" && pathname === "/shiritori") {
         const requestJson = await req.json();
         const nextWord = requestJson.nextWord;
-        if (
+
+        if (//文字が合わない
             nextWord.length > 0 &&
             previousWord.charAt(previousWord.length - 1) !== nextWord.charAt(0)
         ) {
             return new Response("前の単語に続いていません。", { status: 400 });
         }
-        previousWord = nextWord;
+
+        if (//すでに使われている
+            wordsList.includes(nextWord)
+        ) {
+            return new Response("すでに使われている単語です。", { status: 400 });
+        }
+
+        if(//んで終わっている
+            nextWord.charAt(nextWord.length - 1) === "ん"
+        ) {
+            return new Response("「ん」で終わっています。", { status: 400 });
+        }
+
+        //文字列型の配列に追加
+        wordsList.push(nextWord);
+        console.log(wordsList);
+        previousWord = wordsList[wordsList.length - 1];
         return new Response((previousWord),{post: 8000});
     }
 
